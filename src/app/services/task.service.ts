6eb8a2190aea7067bb41d44b87cfd171d836798c
Gavin Corkery <GavinCorkery@hotmail.com>
2021-11-23 16:20:25 -0500

    Made onDblClick event to toggle reminder status of a task + update DB to reflect

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,6 +3,12 @@ import { Observable } from 'rxjs'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Task } from 'src/app/Task';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,8 +23,13 @@ export class TaskService {
   }
 
   deleteTask(task: Task): Observable<Task> {
-    const url = `${this.apiUrl}/${task.id}`
+    const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
   }
 
 }

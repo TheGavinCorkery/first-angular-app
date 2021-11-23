0636a671851cd668500936c1dadf35ab0770f943
Gavin Corkery <GavinCorkery@hotmail.com>
2021-11-23 11:28:36 -0500

    Created component for header and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+//Declaration
 @Component({
+  //Tag used to embed component
   selector: 'app-root',
+  //Link to template HTML
   templateUrl: './app.component.html',
+  //Link to css file
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}

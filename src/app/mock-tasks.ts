08e93d667f93044ae41d2879c1f84d928a58ddd2
Gavin Corkery <GavinCorkery@hotmail.com>
2021-11-23 11:45:43 -0500

    Added dynamic onClick event for button + create Task model and data

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task'
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'November 22nd at 1:40pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Phone Screening',
+    day: 'November 23rd at 1:00pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Grocery Shopping',
+    day: 'November 23rd at 2:30pm',
+    reminder: false,
+  },
+];
